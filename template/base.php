cc344443d91407134eefc0d62062daeac248623f
Sofia <irenesofia.lotti@studio.unibo.it>
2025-09-12 12:44:40 +0200

    WIP: implemented index page

--- template/base.php
+++ template/base.php
@@ -4,31 +4,57 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?php echo $templateParams["titolo"]; ?></title>
-        <link rel="stylesheet" type="text/css" href="../assets/css/style.css?v=46857" />
+        <link rel="stylesheet" type="text/css" href="../assets/css/style.css?v=4687" />
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     
     </head>
 
-    <body>
-        <header>
-            <h1>header</h1>
+    <body class="align-items-center bg-light">
+        <header class="mb-4">
+            <nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom shadow-sm">
+                <div class="container-fluid">
+                    
+                    <a class="navbar-brand fw-bold" href="index.php">
+                    🤖 GuessTheBot
+                    </a>
+
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav ms-auto p-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="classifica.php">Classifica</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="come-si-gioca.php">Come si gioca</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </header>
         
-        <main>
+        <!--main che per ora richiamo in ogni file-->
             <?php
             require($templateParams["nome"]);
             ?>
-        </main>
+        <!-- fine main -->
 
-        <footer>
+        <footer class="text-center text-muted mt-4">
             <p>Irene Sofia Lotti 2025</p>
             <p>Progetto di Tesi (Unibo)</p>
-            <ul class="p-0">
+            <!--<ul class="p-0">
                 <li>FAQ</li>
                 <li>Privacy</li>
                 <li>Terms</li>
-            </ul>
+            </ul>-->
         </footer>
 
 
---
+++ template/contenutoIndex.php
@@ -0,0 +1,33 @@
+<main class="container">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-6 col-lg">
+
+            <div class="card shadow-sm border-0">
+                <div class="card-body p-4 p-md-5 text-center row">
+                    <div class=" col-lg-4">
+                        <img src="../assets/images/logo.jpg" alt="GuessTheBot Logo" class="mb-4 col-12" />
+                    </div>
+                    <div class="align-items-center col-lg-8">
+                        <h1 class="fw-bold my-3 my-lg-5">GUESS THE BOT</h1>
+
+                        <p class="text-muted my-4 my-lg-5">
+                            Metti alla prova la tua capacità di distinguere un testo scritto da un'intelligenza artificiale!
+                        </p>
+
+                        <form action="php/start_game.php" method="POST">
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="playerName" name="playerName" placeholder="Il tuo nome" required>
+                                <label for="playerName">Inserisci il tuo nome</label>
+                            </div>
+                            
+                            <div class="d-grid">
+                                <button class="my-3" type="submit">Gioca</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</main>
